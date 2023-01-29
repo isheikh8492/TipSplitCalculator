96d724bf7bae90af00ea805df6c5a52cf5be7218
isheikh8492 <isheikh8492@gmail.com>
2023-01-29 14:49:47 -0600

    added tip/bill calculation complete + tested

--- app/src/main/java/com/imadudinsheikh/tipsplitcalculator/MainActivity.java
+++ app/src/main/java/com/imadudinsheikh/tipsplitcalculator/MainActivity.java
@@ -3,11 +3,18 @@ package com.imadudinsheikh.tipsplitcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
     private RadioGroup tipPercentageInput;
     private EditText noOfPeopleInput;
 
+    private TextView tipAmountOutput;
+    private TextView totalWithTipOutput;
     private EditText finalBillOutput;
 
     @Override
@@ -25,23 +34,100 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         totalBillInput = findViewById(R.id.billTotalTaxInput);
+
+        TextWatcher textWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                // Do something before text changes
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String input = charSequence.toString();
+                if (input.contains(".")) {
+                    int decimalIndex = input.indexOf(".");
+                    int length = input.length();
+                    if (length - decimalIndex > 3) {
+                        input = input.substring(0, decimalIndex + 3);
+                        totalBillInput.setText(input);
+                        totalBillInput.setSelection(input.length());
+                    }
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // Do something after text changes
+            }
+        };
+        totalBillInput.addTextChangedListener(textWatcher);
+
         tipPercentageInput = findViewById(R.id.tipRadioGroup);
+
+        tipAmountOutput = findViewById(R.id.tipAmountTextView);
+        totalWithTipOutput = findViewById(R.id.totalTipTextView);
         noOfPeopleInput = findViewById(R.id.numberPeopleInput);
+
+        noOfPeopleInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+            @Override
+            public void afterTextChanged(Editable s) {
+                // Remove any non-integer characters from the string
+                String updatedString = s.toString().replaceAll("[^\\d]", "");
+                // If the updated string is different from the original string
+                if (!s.toString().equals(updatedString)) {
+                    // Set the updated string as the new text in the EditText
+                    noOfPeopleInput.setText(updatedString);
+                    // Move the cursor to the end of the updated string
+                    noOfPeopleInput.setSelection(updatedString.length());
+                }
+            }
+        });
+
         finalBillOutput = findViewById(R.id.finalBillOutput);
     }
 
-    public void calculate(View v) {
+    public void calculateTipAndTotalWithTip(View v) {
         int radioButtonId = tipPercentageInput.getCheckedRadioButtonId();
-        Log.d(TAG, "value of radioGroup " + radioButtonId);
-        float result = 0;
+        BigDecimal totalBillWithTax = new BigDecimal(totalBillInput.getText().toString());
+        BigDecimal tipPercentage;
+        BigDecimal tipAmount;
         if ((radioButtonId != -1) && (!Float.isNaN(Float.parseFloat(totalBillInput.getText().toString())))){
-            RadioButton selectedTipAmount = (RadioButton) tipPercentageInput.findViewById(radioButtonId);
-            float bill = Float.parseFloat(totalBillInput.getText().toString());
-            bill += (0.01 * Float.parseFloat(selectedTipAmount.getText().subSequence(0, 2).toString())) * bill;
-            result = (float) bill / Float.parseFloat(noOfPeopleInput.getText().toString());
-            finalBillOutput.setText(Float.toString(result));
+            RadioButton selectedTipAmount = tipPercentageInput.findViewById(radioButtonId);
+            tipPercentage = new BigDecimal(selectedTipAmount.getText().subSequence(0, 2).toString()).multiply(BigDecimal.valueOf(0.01));
+            tipAmount = totalBillWithTax.multiply(tipPercentage).setScale(2, RoundingMode.HALF_UP);
+            BigDecimal totalWithTipAmount = totalBillWithTax.add(tipAmount).setScale(2, RoundingMode.HALF_UP);
+            tipAmountOutput.setText(String.format("$%.2f", tipAmount));
+            totalWithTipOutput.setText(String.format("$%.2f", totalWithTipAmount));
+            totalBillInput.setText(String.format("$%.2f", totalBillWithTax));
         }
-
     }
 
+    public void calculateTotalPerPerson(View v) {
+        int radioButtonId = tipPercentageInput.getCheckedRadioButtonId();
+        BigDecimal totalBillWithTax;
+        BigDecimal tipPercentage;
+        BigDecimal tipAmount;
+        BigDecimal totalWithTipAmount = null;
+        if ((radioButtonId != -1) && (!Float.isNaN(Float.parseFloat(totalBillInput.getText().toString().substring(1))))){
+            RadioButton selectedTipAmount = tipPercentageInput.findViewById(radioButtonId);
+            totalBillWithTax = new BigDecimal(totalBillInput.getText().toString().substring(1));
+            tipPercentage = new BigDecimal(selectedTipAmount.getText().subSequence(0, 2).toString()).multiply(BigDecimal.valueOf(0.01));
+            tipAmount = totalBillWithTax.multiply(tipPercentage).setScale(2, RoundingMode.HALF_UP);
+            totalWithTipAmount = totalBillWithTax.add(tipAmount).setScale(2, RoundingMode.HALF_UP);
+            tipAmountOutput.setText(String.format("$%.2f", tipAmount));
+            totalWithTipOutput.setText(String.format("$%.2f", totalWithTipAmount));
+        }
+        if (noOfPeopleInput.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Number of People input field is empty", Toast.LENGTH_SHORT).show();
+        } else {
+            assert totalWithTipAmount != null;
+            BigDecimal noOfPeople = new BigDecimal(noOfPeopleInput.getText().toString());
+            BigDecimal totalDividedByFour = totalWithTipAmount.divide(noOfPeople, 2, RoundingMode.HALF_UP);
+            finalBillOutput.setText(String.format("$%.2f", totalDividedByFour));
+        }
+    }
 }
